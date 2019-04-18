7a42e78e9387e9cee9985b5f95535d8c2bbca52d
Sebastián Gurín <sgurin@awalabs.com>
2019-04-18 02:28:18 -0300

    virtual

---
+++ guides/virtual-elements.md
@@ -0,0 +1,156 @@
+This is a concept that is not in react...It makes much sencse sence with typescript ... 
+
+Besides react fragments all JSX.Elements declared render a DOM / blessed element. From bottom to the top. 
+
+Virtual element let authors declare elements that 
+authors define  complex data semantics usin gthe markup (with typescript and properties  you ccan validate and define a lot)
+    
+
+Example: A Tab Panel (types definitions as JSX ELement Props)
+
+## Componnet decarations 
+
+(extend VirtualComponent then they wont be rendered (wont create any elememt only provide parent with data.))
+
+```jsx
+class TabHeadings extends VirtualComponent<TabHeadingsProps>{ }
+class TabBlocks extends VirtualComponent<TabBlocksProps>{ }
+class TabBlock extends VirtualComponent<TabBlockProps>{ }
+class TabHeading extends VirtualComponent<TabHeadingProps>{ }
+```
+
+## Props declarations: 
+
+Component Props, . where the semantics 
+
+The API semantics & structure is really defined here (the markup the user needs to write)
+
+```tsx
+interface TabPanelProps {
+  /** Only must contain only TabHeadings and  TabBlocks children,and only one of each. */
+  children: (TabHeadings | TabBlocks)[] 
+}
+/** only alows  TabBlock there must be  */
+interface TabBlocksProps {
+  children: JSX.BlessedJsxNode | TabBlock[]
+}
+interface TabBlockProps {
+  id:  string, children: JSX.BlessedJsxNode
+}
+interface TabHeadingsProps {
+  children: JSX.BlessedJsxText | TabHeading[]
+}
+/** inner htext is mandatoryy and only text. id attr is mandatory. */
+interface TabHeadingProps {
+  active?: boolean, focus?: boolean, id: string, children: JSX.BlessedJsxNode
+}
+```
+## Users code
+It needs to respect Props structure or fail to compile
+
+```tsx
+const UserApp2 = <Div>
+Welcome to the soper heroe store, use the tabs below to focus on special super powers.
+Here we are inside real blessed components, container box for exampe. But 
+below (TabPanel> are virtual components that won't be rendered at all. Probably other elements wil be rendered instead
+<TabPanel>
+  <TabHeadings>
+    <TabHeading id="t1" active={true}>Removint this text gives an error</TabHeading>
+    <TabHeading id="id is mandatory">Ttiel1 </TabHeading>
+    there could be text or spaces between Headings     but not other elements not even intrinsic
+    so uncommenting the next will give error _
+    {/* <text content="elements"/> */}
+  </TabHeadings>
+  <TabBlocks>
+    <TabBlock id="12"> blocks must have text and can contain any other kind of element</TabBlock>
+
+    There cannot be any element besides text between blocks so uncomenting the follwoiing gives
+  {/* <button content=""incalud></button> */}
+    <TabBlock id="2">id is mandatory</TabBlock>
+  </TabBlocks>>
+    {/* There cannot be text between TabBlocks and TabPanel asd123 123 
+    uncommenting this text vigeserror*/}
+</TabPanel>
+</Div>
+```
+
+## The actual implementation
+
+we implemetnt it we hyst a couple of blessing elements 
+
+```jsx
+class TabPanel extends Component<TabPanelProps>{
+  render() {
+    return <Div>
+      Welcome to the soper heroe store, use the tabs below to focus on special super powers.
+        <listbar commands={[]}></listbar>
+      <Div>
+      </Div>
+    </Div>
+  }
+}
+```
+
+  
+
+# TIPS for TypeScript begginrs
+
+
+* Notice that COmponent's accept above Props and returns a much simpler structure (only blessed elements that **will consume the user input, structure, attributes, cgiuldrem etc) as input data / optinos:
+
+ * union with JSX.BlessedJsxNode will include elemnts 
+ * unioin with  JSX.BlessedJsxText to include text 
+ * Children validation TIP: 
+ * {}  no children alowed or text
+ * children: JSX.BlessedJsxNode exactly one children , of any type
+ * children [A, B, C] : exacly three childrens and their types must have that order.
+ * children A|B|C|JSX.BlessedJsxText any of a, b, v or text, in any order any amount
+ *  children: Type | JSX.BlessedJsxNode will allow children of 
+ * Type Union with `JSX.BlessedJsxText``to allow Text (string) children
+
+
+# Bless options 
+
+in blessed feets very well with transversal convepts like border and scrollbar since it allows to separate that seantics with the elelnet:
+Currently is all mixed in the same declaration object - and spreaded ...  at the same time.. 
+
+
+## Proposal : Children eleemnt affects all descendants of its parent:
+
+```tsx
+const userCode =       <ListTable>
+  <Scroll></Scroll>                               <---- .... add a ScrollBar to the list table parent
+  <THead style={{border: {}, backgroud: '', }>     <--- the style is only for the head
+<tr><th>title1</th><th>title2</th></tr>>
+  </THead>
+  <tbody>    
+  <style....>                                   <---- style for cels , spanm etc
+    <tr><td>value1</td><td>value2</td></tr>
+  </tbody>
+</listTable>
+<Form>
+<Focus siblings keys="...."  style=""  >   <-- all siblings
+<Input>Button>
+</form>
+                                                <---- no more focus
+  other content
+  </box>
+```
+
+## Proposal: React-like Providers
+
+(although react's care musch simpler. (Cannot be nested) or doesn't contain qualifiers like these)
+
+Wrap elements to affect options of children: 
+
+```tsx
+<Options set={...baseOptions}>
+  <box>
+  <Options assign={focusableOptions}><textarea></textarea<</Options>
+  <box>el asdlkk selfajkejf kjlka ef
+  l ksdjf s d<Options union={only the union of scope options and these}>something/ioptoins>
+
+  akls jdlkajs kdkksjd aljk <Options remove={fpcisable}>I dont want focus</Options>
+  </box>
+</Options>
+´´´
--- spec/virtualElementSpec.tsx
+++ spec/virtualElementSpec.tsx
@@ -1,202 +1,307 @@
 import { tryTo } from 'misc-utils-of-mine-generic'
-import { Button, createScreen, Div, installExitKeys, React, Screen, Component } from '../src'
+import { Button, createScreen, Div, installExitKeys, React, Screen, Component, ElementOptions } from '../src'
 import { waitFor } from '../src/blessed/waitFor'
 import { VirtualComponent } from '../src/blessed/virtualElement';
 import { stringify } from 'querystring';
+import { style } from 'ansi-escape-sequences';
 
 describe('virtualElements', () => {
-  describe('basic', () => {
+
+  describe('Format, syntax and validation', () => {
     let screen: Screen
     afterEach(() => {
       tryTo(() => screen.destroy())
     })
-    it('should declare data in the markcup consumed by the parent and dont be rendered', async done => {
-      // TODO: later we can impl a abstracthelper component class so virtual component parents can extends and obtain the data transparently
-      class TabPanel extends Component<TabPanelProps>{
-        // a real parent that declares some semantic virtual children like TaB, Body, title... 
-        // the complex this.props structure will be implementing with VirtualChildren instances 
+    // the followin a rich GUI definition that allows th users to declare the semantics using  multiple/arbitrary levels of the markkup . The inmplemention will use few or non of these s blessenodes wejust extracrt the definitions and use virtual node to simulate the blesseing tree.
+    /**
+     * tab panel containig headinsg clickable lanels on top and blocks of content at the bottom.  Hedasings and blocks are associated with an id attribute. 
+     */
+    class TabPanel extends Component<TabPanelProps>{
+      render() {
+        return <Div>
+          Welcome to the soper heroe store, use the tabs below to focus on special super powers.
+            <listbar commands={[]}></listbar>
+          <Div>
+          </Div>
+        </Div>
+      }
+    }
+    // componnet decarations (extend VirtualComponent then they wont be rendered (wont create any elememt only provide parent with data.))
+    class TabHeadings extends VirtualComponent<TabHeadingsProps>{ }
+    class TabBlocks extends VirtualComponent<TabBlocksProps>{ }
+    class TabBlock extends VirtualComponent<TabBlockProps>{ }
+    class TabHeading extends VirtualComponent<TabHeadingProps>{ }
+
+    // component Props, . where the semantics / API is really defined (validated)
+    interface TabPanelProps {
+      /** Only must contain only TabHeadings and  TabBlocks children,and only one of each. */
+      children: (TabHeadings | TabBlocks)[] 
+    }
+    /** only alows  TabBlock there must be  */
+    interface TabBlocksProps {
+      children: JSX.BlessedJsxNode | TabBlock[]
+    }
+    interface TabBlockProps {
+      id:  string, children: JSX.BlessedJsxNode
+    }
+    interface TabHeadingsProps {
+      children: JSX.BlessedJsxText | TabHeading[]
+    }
+    /** inner htext is mandatoryy and only text. id attr is mandatory. */
+    interface TabHeadingProps {
+      active?: boolean, focus?: boolean, id: string, children: JSX.BlessedJsxNode
+    }
+
+
+
+    it('should allow authors define semantics with arbirary markyp and validate children and attributes fleible', async done => {
+      const UserApp2 = <Div>
+        Welcome to the soper heroe store, use the tabs below to focus on special super powers.
+        Here we are inside real blessed components, container box for exampe. But below (TabPanel> are virtual components that won't be rendered at all. Probably other elements wil be rendered instead
+        <TabPanel>
+          <TabHeadings>
+            <TabHeading id="t1" active={true}>Removint this text gives an error</TabHeading>
+            <TabHeading id="id is mandatory">Ttiel1 </TabHeading>
+            there could be text or spaces between Headings     but not other elements not even intrinsic
+            so uncommenting the next will give error _
+             {/* <text content="elements"/> */}
+          </TabHeadings>
+          <TabBlocks>
+            <TabBlock id="12"> blocks must have text and can contain any other kind of element</TabBlock>
+            There cannot be any element besides text between blocks so uncomenting the follwoiing gives
+          {/* <button content=""incalud></button> */}
+            <TabBlock id="2">id is mandatory</TabBlock>
+          </TabBlocks>>
+            {/* There cannot be text between TabBlocks and TabPanel asd123 123 
+            uncommenting this text vigeserror*/}
+        </TabPanel>
+      </Div>
+    })
+
+    describe('rendering', () => {
+      let screen: Screen
+      afterEach(() => {
+        tryTo(() => screen.destroy())
+      })
+      /**
+      Let's build now a nicer declaration for  current blessing List implementation  options. Users will c
+      reate tons of virtual elements, and we will render all witn only one List :). For me current 
+      is kind of spreaded ... I would like somthing like this: (html table simlar)
+    
+
+        header labels, and style
+       * 
+// label, 
+const userCode =       <ListTable>
+  <Scroll></Scroll>                       <---- we can have a semantic element that ust adds options for scollbarm- instead of attribtues....
+  <THead style={{border: {}, backgroud: '', }> <--- the style is only for the head
+<tr><th>title1</th><th>title2</th></tr>>
+  </THead>
+  <tbody>                             <---- style for cels , spanm etc
+    <tr><td>value1</td><td>value2</td></tr>
+  </tbody>
+</listTable>
+
+       */
+      class ListTable extends Component<ListProps>{
         render() {
           return <Div>
-            // the following are the real components and intrinsic that qwill implement this widget. normal stuff, consuming some data from ths.props.. 
-            <listbar commands={[]}></listbar>
-            <Div>
-            </Div>
           </Div>
         }
       }
-
-      type NormalEl =JSX.BlessedJsxNode|string|(JSX.BlessedJsxNode|string)
-      // the structure: definition
+      interface ListProps {
+        
+      }
+      // first the props and below the coponents
       interface TabPanelProps {
-        children: JSX.BlessedJsxNode|string|(JSX.BlessedJsxNode|string|TabHeadings|TabBlocks)[]//  ( (TabHeadings|   string|string[]|       TabBlocks) & NormalEl)[]
-        //   )[
-          // TabHeadings,
-        //   TabBlocks
-        // ]
+        children: TabHeadings | TabBlocks[]
       }
       interface TabBlockProps {
-        id?: string
-        children: JSX.BlessedJsxNode|string|(JSX.BlessedJsxNode|string)[]
-        // active?: boolean
+        id?: string, children: JSX.BlessedJsxNode
       }
       interface TabHeadingsProps {
-        children: (JSX.BlessedJsxNode|string|TabHeading|(JSX.BlessedJsxNode|string))[]
+        children: JSX.BlessedJsxText | TabHeading[]
       }
       interface TabBlocksProps {
-        children: (TabBlock| string[]|string|JSX.BlessedJsxNode|JSX.BlessedJsxNode[])[]
+        children: JSX.BlessedJsxNode | TabBlock[]
       }
       interface TabHeadingProps {
-         active?: boolean, focus?: boolean, id: string, children: string[]|string
+        active?: boolean, focus?: boolean, id: string, children: JSX.BlessedJsxNode
       }
-      class TabHeadings extends VirtualComponent<TabHeadingsProps>{}
-      class TabBlocks extends VirtualComponent<TabBlocksProps>{}
-      class TabBlock extends VirtualComponent<TabBlockProps>{}
-      class TabHeading extends VirtualComponent<TabHeadingProps>{}
+      class TabHeadings extends VirtualComponent<TabHeadingsProps>{ }
+      class TabBlocks extends VirtualComponent<TabBlocksProps>{ }
+      class TabBlock extends VirtualComponent<TabBlockProps>{ }
+      class TabHeading extends VirtualComponent<TabHeadingProps>{ }
+      // betweem represetation of styles
 
-      // that our API now the user code
+      it('should allow authors define semantics with arbitrary markyp and validate children and attributes fleible', async done => {
+        // const UserApp2 = <Div>
+        //   Welcome to the soper heroe store, use the tabs below to focus on special super powers.
+        //   Here we are inside real blessed components, container box for exampe. But below (TabPanel> are virtual components that won't be rendered at all. Probably other elements wil be rendered instead
+        //             <TabPanel>
+        //     <TabHeadings>
+        //       <TabHeading id="t1" active={true}>Removint this text gives an error</TabHeading>
+        //       <TabHeading id="id is mandatory">Ttiel1 </TabHeading>
+        //       there could be text or spaces between Headings     but not other elements not even intrinsic
+        //       so uncommenting the next will give error _
+        //                  {/* <text content="elements"/> */}
+        //     </TabHeadings>
+        //     <TabBlocks>
+        //       <TabBlock id="12"> blocks must have text and can contain any other kind of element</TabBlock>
 
-type WithOtherElementsInBettwen<T> = JSX.BlessedJsxNode|string|(JSX.BlessedJsxNode|string)
+        //       There cannot be any element besides text between blocks so uncomenting the follwoiing gives
+        //               {/* <button content=""incalud></button> */}
+        //       <TabBlock id="2">id is mandatory</TabBlock>
+        //     </TabBlocks>>
+        //                 {/* There cannot be text between TabBlocks and TabPanel asd123 123 
+        //                 uncommenting this text vigeserror*/}
+        //   </TabPanel>
+        // </Div>
+      })
 
+      xit('should not render virtual elements or text inside them', ()=>{})
 
-      const UserApp2 = <Div>
-        <TabPanel>
-          <TabHeadings>
-            <TabHeading  id="t1" active={true}>label  mandatory</TabHeading><TabHeading id="t1">Ttiel1 </TabHeading></TabHeadings>
-<TabBlocks><TabBlock id="12">    dsf sdf
- </TabBlock><TabBlock id="2"> required text</TabBlock></TabBlocks>></TabPanel>
-</Div>
-
-
-      // const UserApp = <Div>
-      //   <TabPanel>
-      // <TabHeadings>
-      //   <TabHeading label="t1" id="t1" active></TabHeading>
-      //   <TabHeading label="t1" id="t1"></TabHeading>
-      //   </TabHeadings>
-      //   <TabBlocks>
-      //     <TabBlock id="12">    dsf sdf </TabBlock>
-      //     <TabBlock id="2"> required text</TabBlock>
-      //     </TabBlocks>>
-      //   </TabPanel>
-      // </Div>
-      
-      
-      // class Tab extends VirtualComponent<TabHeadingsProC>
-
-
-      // now the virtual defininitions
-      // createVir
-      // Now user code: 
-
-
-// class A = V<{}>({})
-//       function V<P>(props: P) {
-//         return class Virtual extends VirtualComponent<P> {
-//         }
-//         // return VirtualComponent.createVirtualComponent(props)
-//       }
-      // class V extends VirtualComponent{}
-
-      // class VirtualParent<T> extends Component<P>{
-      //   render(){
-      //     return <V></V>
-      //   }
-      // }
-
-
-      // class C{
-      //   m(){}
-      // }
-
-      // Object.exC.prototype
-      // function F(){
-      // this.a=1
-      // }
-
-
-      // let proto = new Proxy({}, {
-      //   get(target, propertyKey, receiver) {
-      //       console.log('GET '+propertyKey);
-      //       return target[propertyKey];
-      //   }
-      // });
-
-      // let obj = Object.create(proto);
-      // obj.bla;
-
-
-
-      // interface Blocks
-      // interface  Tagb{
-      //   id: string
-      //   title: string
-      //   children?: [IconProps]
-      // }
-      // interface TabProps {
-      //   name: string
-      //   active?: boolean
-      //   // children
-      // }
-      // class TabComponent extends VirtualComponent<TabProps> {
-      //   getVirtualData(){
-      //     return this.props
-      //   }
-      // }
-      //   screen = createScreen({ smartCSR: true, log: 'log.txt', fullUnicode: true })
-      //   installExitKeys(screen)
-      //   const ref1 = React.createRef<Button>()
-      //   React.render(
-      //     <Div width="100%" border="line" height="100%" style={{ bg: 'red' }} parent={screen}>
-      //       <button
-      //         ref={ref1}
-      //         top="80%"
-      //         left="80%"
-      //         content="button11"
-      //         onPress={e => {
-      //           e.currentTarget.content = 'clicked!'
-      //           e.currentTarget.screen.render()
-      //         }}
-      //       />
-      //     </Div>
-      //   )
-      //   screen.render()
-      //   ref1.current!.content = "'changed3"
-      //   ref1.current!.screen.render()
-      //   await waitFor(() => ref1.current! && ref1.current!.getContent().includes('changed3'))
-      //   done()
-      // })
-    })
-
-    xit('should allow an eelmetn to decalre virual childs just using props.children', async done => {
-      interface P {
-        children: (TabProps | BlockProps)[]
-      }
-      interface TabProps {
-        id: string
-        title: string
-        children?: [IconProps]
-      }
-      interface IconProps { url: string }
-      interface BlockProps {
-        id?: string
-        paras: string[]
-      }
-      class TabPanel extends Component<P>{
-        render() {
-          return <Virtual {...this.props} />
-        }
-      }
-      type TabPropsUnion = P | TabProps | IconProps | BlockProps
-      class Virtual extends VirtualComponent<TabPropsUnion> {
-
-      }
+      xit('what anpit on virtual elemetnrs inside girtual elemnts ?', ()=>{})
 
     })
-
 
 
   })
+
+
+  // type WithOtherElementsInBetween<T> = JSX.BlessedJsxNode | string | (JSX.BlessedJsxNode | string)
+  // type NormalEl =JSX.BlessedJsxNode|string|(JSX.BlessedJsxNode|string)
+  // the structure: definition
+  // const UserApp = <Div>
+  //   <TabPanel>
+  // <TabHeadings>
+  //   <TabHeading label="t1" id="t1" active></TabHeading>
+  //   <TabHeading label="t1" id="t1"></TabHeading>
+  //   </TabHeadings>
+  //   <TabBlocks>
+  //     <TabBlock id="12">    dsf sdf </TabBlock>
+  //     <TabBlock id="2"> required text</TabBlock>
+  //     </TabBlocks>>
+  //   </TabPanel>
+  // </Div>
+
+
+  // class Tab extends VirtualComponent<TabHeadingsProC>
+
+
+  // now the virtual defininitions
+  // createVir
+  // Now user code: 
+
+
+  // class A = V<{}>({})
+  //       function V<P>(props: P) {
+  //         return class Virtual extends VirtualComponent<P> {
+  //         }
+  //         // return VirtualComponent.createVirtualComponent(props)
+  //       }
+  // class V extends VirtualComponent{}
+
+  // class VirtualParent<T> extends Component<P>{
+  //   render(){
+  //     return <V></V>
+  //   }
+  // }
+
+
+  // class C{
+  //   m(){}
+  // }
+
+  // Object.exC.prototype
+  // function F(){
+  // this.a=1
+  // }
+
+
+  // let proto = new Proxy({}, {
+  //   get(target, propertyKey, receiver) {
+  //       console.log('GET '+propertyKey);
+  //       return target[propertyKey];
+  //   }
+  // });
+
+  // let obj = Object.create(proto);
+  // obj.bla;
+
+
+
+  // interface Blocks
+  // interface  Tagb{
+  //   id: string
+  //   title: string
+  //   children?: [IconProps]
+  // }
+  // interface TabProps {
+  //   name: string
+  //   active?: boolean
+  //   // children
+  // }
+  // class TabComponent extends VirtualComponent<TabProps> {
+  //   getVirtualData(){
+  //     return this.props
+  //   }
+  // }
+  //   screen = createScreen({ smartCSR: true, log: 'log.txt', fullUnicode: true })
+  //   installExitKeys(screen)
+  //   const ref1 = React.createRef<Button>()
+  //   React.render(
+  //     <Div width="100%" border="line" height="100%" style={{ bg: 'red' }} parent={screen}>
+  //       <button
+  //         ref={ref1}
+  //         top="80%"
+  //         left="80%"
+  //         content="button11"
+  //         onPress={e => {
+  //           e.currentTarget.content = 'clicked!'
+  //           e.currentTarget.screen.render()
+  //         }}
+  //       />
+  //     </Div>
+  //   )
+  //   screen.render()
+  //   ref1.current!.content = "'changed3"
+  //   ref1.current!.screen.render()
+  //   await waitFor(() => ref1.current! && ref1.current!.getContent().includes('changed3'))
+  //   done()
+  // })
 })
+
+//     xit('should allow an eelmetn to decalre virual childs just using props.children', async done => {
+//       interface P {
+//         children: (TabProps | BlockProps)[]
+//       }
+//       interface TabProps {
+//         id: string
+//         title: string
+//         children?: [IconProps]
+//       }
+//       interface IconProps { url: string }
+//       interface BlockProps {
+//         id?: string
+//         paras: string[]
+//       }
+//       class TabPanel extends Component<P>{
+//         render() {
+//           return <Virtual {...this.props} />
+//         }
+//       }
+//       type TabPropsUnion = P | TabProps | IconProps | BlockProps
+//       class Virtual extends VirtualComponent<TabPropsUnion> {
+
+//       }
+
+//     })
+
+
+
+//   })
+// })
 
 // describe('waitFor green full of comments', () => {
 //   let screen: Screen
