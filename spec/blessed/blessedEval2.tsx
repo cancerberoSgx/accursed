dbaacdfc6e0ff0b0c9631c8b298cbc1a3c1ac45c
Sebastián Gurín <sgurin@awalabs.com>
2019-04-26 18:56:19 -0300

    editor-widget

--- spec/blessed/blessedEval2.tsx
+++ spec/blessed/blessedEval2.tsx
@@ -1,9 +1,14 @@
-import { Box, installExitKeys, Div, React, Screen, prompt, Columns, Column , createScreen, Br, Rows, Row, Component, TextareaOptions, Textarea} from '../../src';
-import { debug, inspect } from 'util';
+import { Box, installExitKeys, Div, React, Screen, prompt, Columns, Column, createScreen, Element, Br, Rows, Row, Component, TextareaOptions, debug, Textarea } from '../../src';
+import { inspect } from 'util';
 import { waitFor } from '../../src/blessed/waitFor';
 import * as accursed from '../../src';
+import { TODO } from 'misc-utils-of-mine-typescript';
+import { EventEmitter } from 'events';
+import { renderer, isElement } from '../../dist/src';
+import { sleep } from '../blessedTestUtil';
+const Editor = require('editor-widget')
 
-const focusableOpts:()=>TextareaOptions = ()=>({
+const focusableOpts: () => TextareaOptions = () => ({
   mouse: true,
   keys: true,
   focusable: true,
@@ -32,73 +37,183 @@ const focusableOpts:()=>TextareaOptions = ()=>({
     },
   }
 })
-interface P{
+interface P {
   parent: Screen
 }
 class App extends Component<P> {
   outputEl: Box;
-  editor: Textarea;
+  editor: IEditor;
   logEl: Box;
-  render(){
+  editorContainer: Box;
+  render() {
     setTimeout(() => {
       this.afterRender()
     }, 10);
-    return  <Div parent={this.props.parent}>
-    <Columns>
-    <Column width="50%">
-    <Rows>
-      <Row height="60%">
-    Code Editor<Br/>
-    <textarea label="Blessed Code" {...focusableOpts()} focused={true} width="100%" height="100%" value={examples[0].code} ref={React.createRef<Textarea
-      >(c=>this.editor=c)}></textarea>
-      </Row>  
-      <Row height="40%">
-   Actions<Br/>
-  <button {...focusableOpts()} content="execute" onPress={e=>this.execute()}></button>
-      </Row>{}
-    </Rows>
-    </Column>
-    <Column width="50%">
-    <Rows>
-      <Row height="60%">  
-      <box width="100%" height="100%" label="Output" border="line" ref={React.createRef<Box>(c=>this.outputEl=c)}></box>
-      </Row>  
-      <Row height="40%">
-      <box width="100%" height="100%" label="Log" border="line" ref={React.createRef<Box>(c=>this.logEl=c)}></box>
-      </Row>{}
-    </Rows>
-    </Column>
-    {}
-    </Columns>
+    return <Div parent={this.props.parent}>
+      <Columns>
+        <Column width="50%">
+          <Rows>
+            <Row height="60%">
+              <box width="100%" height="100%" label="Core editor" border="line" focusable={true} ref={React.createRef<Box>(c => this.editorContainer = c)}></box>
+              Code Editor<Br />
+              {/* <textarea label="Blessed Code" {...focusableOpts()} focused={true} width="100%" height="100%" value={examples[0].code} ref={React.createRef<Textarea
+      >(c=>this.editor=c)}></textarea> */}
+            </Row>
+            <Row height="40%">
+              Actions<Br />
+              <button {...focusableOpts()}  name="execute-button" content="execute" onPress={e => this.execute()}></button>
+            </Row>{}
+          </Rows>
+        </Column>
+        <Column width="50%">
+          <Rows>
+            <Row height="60%">
+              <box width="100%" height="100%" label="Output" border="line" ref={React.createRef<Box>(c => this.outputEl = c)}></box>
+            </Row>
+            <Row height="40%">
+              <box width="100%" height="100%" label="Log" border="line" ref={React.createRef<Box>(c => this.logEl = c)}></box>
+            </Row>{}
+          </Rows>
+        </Column>
+        {}
+      </Columns>
     </Div>
   }
   execute(): void {
-    const _log=[]
+    // debug(this.editor, this.editor && this.editor.textBuf)
+    const _log = []
     var options = {
       parent: this.outputEl,
       accursed,
-      log(...args: any[]){
-        _log.push(...args.map(a=>inspect(a)))
+      log(...args: any[]) {
+        _log.push(...args.map(a => inspect(a)))
       }
     }
+    const text = this.editor.textBuf.getText()
+    // debug(text)
     const code = `
     (
-    ${this.editor.getValue()}
+    ${text}
     )(options)`
     try {
       eval(code)
     } catch (error) {
-      
+debug(error)
     }
   }
   async afterRender() {
-    await waitFor(()=>this.outputEl)
+    await waitFor(() => this.editorContainer)
+    
+    const executeButton = this.findDescendant(b=>isElement(b) && b.type==='button' && b.name==='execute-button')
+    executeButton.focus()
+    // const screen = blessed.screen({ smartCSR: true, title: 'editor-widget example' })
+    this.editor = new Editor({
+      // normal blessed widget, use like you would any other blessed element
+      parent: this.editorContainer,
+      // parent: this.screen,
+      ...focusableOpts(),
+      // focused: true,
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      keys: true, 
+      vi: true,
+      keyable: true,
+
+      // buffer: {
+      //   bg: 'green ',
+      //   focusable: true,
+      //   focused: true,
+      //   focus: { bg: 'green' },
+      // },
+      // bg: 'red',
+      styles:{bg: 'red',
+        buffer: {
+          bg: 'green ',
+          // focusable: true,
+          // focused: true,
+          focus: { bg: 'green' }
+,
+
+        },
+
+      },
+      // text: examples[0].code
+    })
+
+    // const changes = this.editor.append('sldkfjhsldkjflskdjf')
+    // this.editor.setText( examples[0].code)
+    // this.editor._updateCursor()
+    // this.editor._updateContent()
+    // this.editor.
+    // this.editor.setLanguageMode({getLanguageId: ()=>'js'})
+    // this.editor.textBuf.emitDidChangeTextEvent()
+    // this.editorContainer.screen.render()
+    // this.editor.buffer.focus()
+    // this.editor.focus()
+    // this.editor.render()
+    // this.editorContainer.screen.render()
+    
+    
+    // this.editor.textBuf.emitWillChangeTextEvent()
+    this.editor.textBuf.setText(examples[0].code)
+    this.editor.language('js')
+    this.editor.toggleInsertMode()
+
+    // this.screen.program.write('\n')
+    // .emit('click', {})
+    // await sleep(1000)
+    // this.editor.textBuf.emitDidChangeTextEvent()
+    // this.editorContainer.screen.emit('key down', undefined, {name: 'down'})
+    // this.editorContainer.screen.emit('key enter', undefined, {name: 'enter'})
+    // this.editor.emit('keypress', {name: 'a'}, {name: 'a'})
+    this.editorContainer.screen.render()
+    // this.editor.
   }
 }
 
+
+interface IEditor extends EventEmitter {
+  focus():void
+  buffer: Element
+  render(): void
+  append(text: string, options?: TODO): TODO
+  _updateCursor(): void
+  _updateContent(): void
+  setLanguageMode(opts: { getLanguageId: () => string }): void
+  open(p: string): Promise<IEditor>
+  save(p: string): Promise<string>
+  undo(options: TODO): TODO
+  redo(options: TODO): TODO
+  textBuf: TextBuffer
+  copy: TODO
+  paste: TODO
+  toggleInsertMode():void
+  
+  language(s: string): void
+  selection: TODO
+}
+/** */
+interface TextBuffer extends EventEmitter {
+  emitDidChangeTextEvent(...args: TODO): TODO
+  getText(): string
+  emitWillChangeTextEvent():void
+  setText(s: string): void
+  // buffer1.onDidChange(({changes}) => {
+  //   for (const {oldRange, newText} of changes.reverse()) {
+  //     buffer2.setTextInRange(oldRange, newText)
+  //   }
+  // })
+
+  //   .getTextInRange({
+  //   start: new Point(scroll.row, 0),
+  //   end: scroll.translate(size)
+  // })))
+}
 const examples = [
   {
-    name: 'simple1', 
+    name: 'simple1',
     code: `
 function simple1(options){
   options.log('starting')
@@ -111,25 +226,27 @@ function simple1(options){
     `
   }
 ]
-function start(){
-try {
-  var screen = createScreen({
-    sendFocus: true
-    // , focusable: true
-  })
-  installExitKeys(screen)
-  screen.key('tab', k => screen.focusNext())
-  screen.key('S-tab', k => screen.focusPrevious())
-  // main(screen)
-  // screen.focusNext()
-  screen.render()
-  const app =React.render(<App parent={screen}></App>)
-  screen.append(app)
-screen.render()
-} catch (error) {
-  debug(error)
+function start() {
+  try {
+    var screen = createScreen({
+      // sendFocus: true, 
+      smartCSR: true, title: 'editor-widget example'
+      // , focusable: true,
+      // tput: true
+    })
+    installExitKeys(screen)
+    screen.key('tab', k => screen.focusNext())
+    screen.key('S-tab', k => screen.focusPrevious())
+    // main(screen)
+    const app = React.render(<App parent={screen}></App>)
+    // screen.append(app)
+    // screen.focusNext()
+    // screen.render()
+    screen.render()
+  } catch (error) {
+    debug(error)
 
-}
+  }
 }
 start()
 
