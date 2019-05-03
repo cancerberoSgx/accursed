cfb608e9f0565f07c121ebb6d0d205ce5d3d68af
Sebastián Gurín <sgurin@awalabs.com>
2019-05-03 00:02:42 -0300

    columns and rows together

--- spec/rowColumnResizeSpec.tsx
+++ spec/rowColumnResizeSpec.tsx
@@ -239,6 +239,319 @@ describe('rowColumnResize', () => {
   })
 
 
+
+  // jasmine.DEFAULT_TIMEOUT_INTERVAL = 99999
+  it('should resize multiple rows and columns int he same container. Also if all uses the same names, installVisibleOnAncestorFocus can be called only once', async done => {
+    try {
+      installVisibleOnAncestorFocus({
+        screen,
+        ancestorPredicate: e => isElement(e) && e.options && e.options._data && e.options._data.rowColumnResize,
+        targetPredicate: (e: Element) => e.name === 'rowColumnResize'
+      })
+      const t1 = (
+        <Div>
+          <Rows>
+            <Row name="column1" height="20%" _data={{ rowColumnResize: { height: 20 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div height={1} name="rowColumnResize" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: false })}>
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true, width: false  })}>
+                  {'>'}
+                </Button2>               
+              </Div>
+
+
+
+
+
+              <Columns>
+            <Column name="column1" bg="yellow" width="20%" _data={{ rowColumnResize: { width: 20 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true  })}>
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true , width: true})}>
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+
+            <Column name="column2" width="50%" bg="magenta" _data={{ rowColumnResize: { width: 50 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" bg="magenta" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true })}
+                  bg="green">
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true, width: true })}
+                  bg="green">
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+            <Column name="column3" width="30%" bg="red" _data={{ rowColumnResize: { width: 30 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" bg="red" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true })}
+                  bg="green">
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true, width: true })}
+                  bg="green">
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+            {}
+          </Columns>
+
+
+
+
+            </Row>
+
+            <Row name="column2" height="50%" bg="cyan" _data={{ rowColumnResize: { height: 50 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div height={1} name="rowColumnResize" bg="cyan" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: false  })}
+                  bg="green">
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true, width: false  })}
+                  bg="green">
+                  {'>'}
+                </Button2>
+              </Div>
+
+              <Columns>
+            <Column name="column1" width="20%" _data={{ rowColumnResize: { width: 20 } }  }bg="gray">
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" hidden={true}bg="gray">
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true  })}>
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true , width: true})}>
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+
+            <Column name="column2" width="50%" bg="magenta" _data={{ rowColumnResize: { width: 50 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" bg="cyan" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true })}
+                  bg="green">
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true, width: true })}
+                  bg="green">
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+            <Column name="column3" width="30%" bg="red" _data={{ rowColumnResize: { width: 30 } }} >
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" bg="red" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true })}
+                  bg="green">
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true, width: true })}
+                  bg="green">
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+            {}
+          </Columns>
+
+
+
+            </Row>
+            <Row name="column3" height="30%" bg="green" _data={{ rowColumnResize: { height: 30 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div height={1} name="rowColumnResize" bg="green" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false , width: false })}
+                  bg="green">
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true , width: false })}
+                  bg="green">
+                  {'>'}
+                </Button2>
+              </Div>
+
+              <Columns>
+            <Column name="column1" width="20%" _data={{ rowColumnResize: { width: 20 } }} bg="orange">
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" hidden={true} bg="orange">
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true  })}>
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true , width: true})}>
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+
+            <Column name="column2" width="50%" bg="magenta" _data={{ rowColumnResize: { width: 50 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" bg="magenta" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true })}
+                  bg="green">
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true, width: true })}
+                  bg="green">
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+            <Column name="column3" width="30%" bg="red" _data={{ rowColumnResize: { width: 30 } }}>
+              <Button2 {...focusableOpts()} onClick={e => {}}>
+                >focusable
+              </Button2>
+              <Div width={10} name="rowColumnResize" bg="red" hidden={true}>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: false, width: true })}
+                  bg="green">
+                  {'<'}
+                </Button2>
+                <Button2
+                  {...focusableOpts()}
+                  border={undefined}
+                  onClick={e => rowColumnResizeHandler({ e: e.currentTarget, increment: true, width: true })}
+                  bg="green">
+                  {'>'}
+                </Button2>
+              </Div>
+            </Column>
+            {}
+          </Columns>
+
+
+
+            </Row>
+            {}
+          </Rows>
+        </Div>
+      )
+      const el = React.render(t1)
+      screen.append(el)
+
+      const l = screenLogger(screen).log
+      l('start')
+      // const logger = installLogger(screen)
+      screen.render()
+      // logger.log('screen rendered')
+      await waitFor(() => printElement(el).includes('focusable'))
+      // 
+      // expect(printElement(screen)).not.toContain('start')
+      // expect(printElement(el)).not.toContain('10-70-20')
+      // expect(printElement(el)).not.toContain('20-10-70')
+
+      // // //TODO: test the handler...
+
+      done()
+    } catch (error) {
+      debug('ERROR', error)
+    }
+  })
+
+
   // jasmine.DEFAULT_TIMEOUT_INTERVAL=99999
   it('an initial experiment', async done => {
     try {
